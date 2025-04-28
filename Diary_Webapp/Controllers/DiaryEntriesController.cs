d367c5aa581a34c4026dd908624635f6bfe05fa7
Aaryan Navare <aaryan.n@isourceinfosystems.com>
2025-04-28 10:10:22 +0530

    FInal changes

--- Diary_Webapp/Controllers/DiaryEntriesController.cs
+++ Diary_Webapp/Controllers/DiaryEntriesController.cs
@@ -4,6 +4,7 @@ using Diary_Webapp.Data; //this is a namespace for the data context
 using Diary_Webapp.Models;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Diary_Webapp.Controllers //this is a namespace for the controller
 {
@@ -36,8 +37,57 @@ namespace Diary_Webapp.Controllers //this is a namespace for the controller
 
             _db.DiaryEntry.Add(obj);
             _db.SaveChanges();
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index");
         }
-        
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.DiaryEntry.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(DiaryEntry obj)
+        {
+
+            _db.DiaryEntry.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.DiaryEntry.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(DiaryEntry obj)
+        {
+
+            _db.DiaryEntry.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
+
 }
+
--- Diary_Webapp/Models/DiaryEntry.cs
+++ Diary_Webapp/Models/DiaryEntry.cs
@@ -10,13 +10,14 @@ namespace Diary_Webapp.Models
         [Key]
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Please enter a Title!")]
+        [StringLength(100, ErrorMessage = "Title cannot be longer than 100 characters.")]
         public required string Title { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Enter your Content")]
         public required string Content { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Enter Date to store record")]
         public DateTime CreatedAt { get; set; }
     }
 
